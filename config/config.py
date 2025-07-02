3f0fd8982007c9e2483685acd1a2b74ed749ac7f
Jean-Paul van Leipsig <jeanpaul.vanleipsig@gmail.com>
2025-07-02 21:57:24 +0100

    Make the host ip a configurable string in the config file.

--- config/config.py
+++ config/config.py
@@ -4,7 +4,11 @@ import os
 load_dotenv()
 
 class Config:
+    # Connection configuration
+    TCP_HOST = os.getenv("TCP_HOST", "192.168.174.251")
     TCP_PORT = int(os.getenv("TCP_PORT", 4000))
+
+    # Database configuration
     DB_NAME = os.getenv("DB_NAME")
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from server import PLCServer
 from config import Config
 
 if __name__ == "__main__":
-    server = PLCServer("0.0.0.0", Config.TCP_PORT)
+    server = PLCServer(Config.TCP_HOST, Config.TCP_PORT)
     server.start()
